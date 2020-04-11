515173e83985dcfe6e6933e088d2ae7773319f51
doanquocnhan <58899942+doanquocnhan@users.noreply.github.com>
2020-04-11 22:59:10 +0700

    V 2.1

---
+++ DoanQuocNhan_59131665_59.CNTT-2/src/doanquocnhan_59131665_59/cntt/pkg2/DoanQuocNhan_59131665_59CNTT2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doanquocnhan_59131665_59.cntt.pkg2;
+
+/**
+ *
+ * @author Nhan
+ */
+public class DoanQuocNhan_59131665_59CNTT2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
