73b17aaf4343e7f5234be1ab7f64fffbe3464ec3
doanquocnhan <58899942+doanquocnhan@users.noreply.github.com>
2020-04-12 09:36:05 +0700

    V 4.0

--- DoanQuocNhan_59131665_59.CNTT-2/src/doanquocnhan_59131665_59/cntt/pkg2/DoanQuocNhan_59131665_59CNTT2.java
+++ DoanQuocNhan_59131665_59.CNTT-2/src/doanquocnhan_59131665_59/cntt/pkg2/DoanQuocNhan_59131665_59CNTT2.java
@@ -5,6 +5,8 @@
  */
 package doanquocnhan_59131665_59.cntt.pkg2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Nhan
@@ -15,7 +17,22 @@ public class DoanQuocNhan_59131665_59CNTT2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+    GiaoVien gvch = new GiaoVien("R- thong ke", "HTTTQL", "Thuan", 50, "Nha Trang", "0161324578") ;
+    
+    LopHoc lopHoc = new LopHoc(gvch);
+    
+    HocSinh hs1 = new HocSinh("59CNTT-1","ve tranh", "Doan Quoc Nhan", 21, "Van Ninh", "0584588366");
+    lopHoc.themHocSinh(hs1);
+    HocSinh hs2 = new HocSinh("60CNTT-1","ve tranh", "Tran Huu Buu", 20, "Van Ninh", "0584588366");
+    lopHoc.themHocSinh(hs2);
+    GiaoVien gv1 = new GiaoVien("Lap trinh Androi", "CNPM", "Tuan Anh", 39, "Nha Trang", "012457893");
+    lopHoc.themGVGD(gv1);
+    GiaoVien gv2 = new GiaoVien("Lap trinh OOP", "CNPM", "Kim Ngoan", 36, "Nha Trang", "0124578934");
+    lopHoc.themGVGD(gv2);
+    
+    
+    lopHoc.inDSHS();
+    lopHoc.inDSGVGD();
     }
     
 }
